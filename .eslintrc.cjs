61c4fccd9ba85c1b79504825c70f29ff3e043a60
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2024-10-31 15:22:55 +0900

    Fix: 에러 방지를 위한 eslint 수정 #3

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,7 +24,7 @@ module.exports = {
     indent: "off",
     "no-undef": "error", // 정의 안 한 변수 사용 x
     "no-trailing-spaces": "error", // 쓸데없는 공백 지우기
-    "import/newline-after-import": ["error", { count: 1 }], // import 구문들 뒤에 한 칸 띄우고 코드 작성
+    // "import/newline-after-import": ["error", { count: 1 }], // import 구문들 뒤에 한 칸 띄우고 코드 작성
     "no-multi-spaces": "error", // 스페이스 여러개 금지
     "no-duplicate-imports": "error", // 중복 Import 금지
   },

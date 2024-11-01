8f19607d7061c2bef4085ffc9d5fd8ab0abbfb72
yelynnn <lynn1833@ewhain.net>
2024-11-01 12:39:59 +0900

    Feat: 라우팅 연결

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import MainPage from '../pages/mainpage';
+import MainPage from '../pages/MainPage';
+
 const router = createBrowserRouter([
   {
     path: '/',

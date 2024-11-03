967a0369e77d40522dd52777225503dddab44d69
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2024-11-03 13:59:32 +0900

    Feat: font 일괄 적용 #3

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  ${reset}
+  
+  * {
+    font-family: 'Pretendard-Regular', sans-serif;
+  }
+
+`;
+
+export default GlobalStyle;

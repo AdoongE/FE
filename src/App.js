fae9f6daeffbbf4a90816189ed92fc38ee25f9ac
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2024-11-04 17:49:00 +0900

    Docs: 겹치는 라우터 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { StyledEngineProvider } from '@mui/styled-engine';
-import Router from './routes/router';
+import Router from './routes/Router';
 import GlobalStyle from './styles/GlobalStyle';
 
 function App() {

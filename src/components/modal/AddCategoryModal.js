f2ddfdeb6bdfd9216ea0d8dd96ea0adc9c41bf75
yelynnn <lynn1833@ewhain.net>
2024-11-11 00:42:13 +0900

    Fix: 머지 충돌 해결

---
+++ src/components/modal/AddCategoryModal.js
@@ -0,0 +1,147 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { Icon } from '@iconify/react';
+import { IOSSwitch } from '../switch/PublicCategorySwitch';
+
+export const AddCategoryModal = ({ isOpen, onClose, onConfirm }) => {
+  const [categoryName, setCategoryName] = useState('');
+  const [isPublic, setIsPublic] = useState(true); // 토글 공개 여부
+
+  const handleToggle = () => {
+    setIsPublic(!isPublic);
+  };
+
+  const handleConfirm = () => {
+    const newCategoryName = categoryName || '새로운 카테고리'; // 입력이 없을 때 추가 내용
+    onConfirm(newCategoryName);
+    setCategoryName('');
+  };
+
+  return (
+    <ModalOverlay>
+      <ModalContent onClick={(e) => e.stopPropagation()}>
+        <ModalDiv>
+          <TopDiv>
+            <ModalTitle>카테고리 추가</ModalTitle>
+            <Icon
+              icon="line-md:close"
+              style={{
+                width: '36px',
+                height: '36px',
+                cursor: 'pointer',
+              }}
+              onClick={onClose}
+            />
+          </TopDiv>
+          <Label>
+            공개 카테고리
+            <IOSSwitch checked={isPublic} onChange={handleToggle} />
+          </Label>
+          <Input
+            value={categoryName}
+            onChange={(e) => setCategoryName(e.target.value)}
+            placeholder=" 카테고리 이름을 입력하세요. (선택)"
+          />
+          <ButtonContainer>
+            <ModalButton className="no" onClick={onClose}>
+              취소
+            </ModalButton>
+            <ModalButton className="ok" onClick={handleConfirm}>
+              확인
+            </ModalButton>
+          </ButtonContainer>
+        </ModalDiv>
+      </ModalContent>
+    </ModalOverlay>
+  );
+};
+
+AddCategoryModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+};
+
+export default AddCategoryModal;
+
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.55);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+`;
+
+const ModalContent = styled.div`
+  background-color: white;
+  border-radius: 50px;
+  width: 46.625rem;
+  height: 21.875rem;
+`;
+
+const ModalDiv = styled.div`
+  margin: 3.125rem;
+`;
+
+const TopDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 27px;
+`;
+
+const ModalTitle = styled.h2`
+  font-size: 32px;
+  font-weight: 850;
+  font-family: 'Pretendard-Regular';
+  margin-bottom: 10px;
+`;
+
+const Label = styled.label`
+  font-size: 22px;
+  font-family: 'Pretendard-Regular';
+  color: #4f4f4f;
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  gap: 11px;
+`;
+
+const Input = styled.input`
+  background-color: #f6f6f6;
+  border: none;
+  border-bottom: 1px solid #7f7f7f;
+  width: 100%;
+  height: 4.25rem;
+  margin-top: 26px;
+  margin-bottom: 18px;
+  font-size: 30px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const ModalButton = styled.button`
+  height: 3.375rem;
+  width: 6.188rem;
+  font-size: 22px;
+  border: none;
+  border-radius: 50px;
+  cursor: pointer;
+  margin-right: 10px;
+  &.ok {
+    background-color: #41c3ab;
+    color: white;
+  }
+  &.no {
+    background-color: #dcdada;
+    color: black;
+  }
+`;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,72 +1,70 @@
-import React, { useState, useEffect, useRef } from "react";
-import { FaBell } from "react-icons/fa";
+import React, { useState, useEffect, useRef } from 'react';
+import { FaBell } from 'react-icons/fa';
 
 function Navbar() {
-const [activeTab, setActiveTab] = useState("");
-const [activeBarWidth, setActiveBarWidth] = useState(0);
-const [activeBarLeft, setActiveBarLeft] = useState(0);
-const navbarMenuRef = useRef(null);
+  const [activeTab, setActiveTab] = useState('');
+  const [activeBarWidth, setActiveBarWidth] = useState(0);
+  const [activeBarLeft, setActiveBarLeft] = useState(0);
+  const navbarMenuRef = useRef(null);
 
-const handleTabClick = (tabName, event) => {
-setActiveTab(tabName);
-const button = event.currentTarget;
-const { offsetWidth, offsetLeft } = button;
-setActiveBarWidth(offsetWidth);
-setActiveBarLeft(offsetLeft);
-};
+  const handleTabClick = (tabName, event) => {
+    setActiveTab(tabName);
+    const button = event.currentTarget;
+    const { offsetWidth, offsetLeft } = button;
+    setActiveBarWidth(offsetWidth);
+    setActiveBarLeft(offsetLeft);
+  };
 
-useEffect(() => {
-const firstButton = navbarMenuRef.current.querySelector("button");
-if (firstButton) {
-    firstButton.click();
-}
-}, []);
+  useEffect(() => {
+    const firstButton = navbarMenuRef.current.querySelector('button');
+    if (firstButton) {
+      firstButton.click();
+    }
+  }, []);
 
-return (
-<div>
-    <div className="navbar">
-    <div className="navbar-logo-container">
-        <div className="navbar-logo-box"></div>
-        <div className="navbar-logo">seedzip</div>
+  return (
+    <div>
+      <div className="navbar">
+        <div className="navbar-logo-container">
+          <div className="navbar-logo-box"></div>
+          <div className="navbar-logo">seedzip</div>
+        </div>
+        <div className="navbar-menu" ref={navbarMenuRef}>
+          <button
+            onClick={(e) => handleTabClick('모아보기', e)}
+            className={activeTab === '모아보기' ? 'active' : ''}
+          >
+            모아보기
+          </button>
+          <button
+            onClick={(e) => handleTabClick('관리하기', e)}
+            className={activeTab === '관리하기' ? 'active' : ''}
+          >
+            관리하기
+          </button>
+          <button
+            onClick={(e) => handleTabClick('탐색하기', e)}
+            className={activeTab === '탐색하기' ? 'active' : ''}
+          >
+            탐색하기
+          </button>
+          <div
+            className="active-bar"
+            style={{
+              width: activeBarWidth,
+              left: activeBarLeft,
+            }}
+          />
+        </div>
+        <div className="navbar-right">
+          <FaBell className="navbar-icon" />
+          <button className="navbar-new-content">+ 새 콘텐츠</button>
+          <div className="navbar-profile"></div>
+        </div>
+      </div>
+      <div className="sidebar">{/* 사이드바 내용 */}</div>
     </div>
-    <div className="navbar-menu" ref={navbarMenuRef}>
-        <button
-        onClick={(e) => handleTabClick("모아보기", e)}
-        className={activeTab === "모아보기" ? "active" : ""}
-        >
-        모아보기
-        </button>
-        <button
-        onClick={(e) => handleTabClick("관리하기", e)}
-        className={activeTab === "관리하기" ? "active" : ""}
-        >
-        관리하기
-        </button>
-        <button
-        onClick={(e) => handleTabClick("탐색하기", e)}
-        className={activeTab === "탐색하기" ? "active" : ""}
-        >
-        탐색하기
-        </button>
-        <div
-        className="active-bar"
-        style={{
-            width: activeBarWidth,
-            left: activeBarLeft,
-        }}
-        />
-    </div>
-    <div className="navbar-right">
-        <FaBell className="navbar-icon" />
-        <button className="navbar-new-content">+ 새 콘텐츠</button>
-        <div className="navbar-profile"></div>
-    </div>
-    </div>
-    <div className="sidebar">
-    {/* 사이드바 내용 */}
-    </div>
-</div>
-);
+  );
 }
 
 export default Navbar;
@@ -193,7 +191,7 @@ z-index: 1;
 `;
 
 // Adding styles to document
-const styleSheet = document.createElement("style");
-styleSheet.type = "text/css";
+const styleSheet = document.createElement('style');
+styleSheet.type = 'text/css';
 styleSheet.innerText = styles;
 document.head.appendChild(styleSheet);

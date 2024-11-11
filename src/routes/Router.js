81b6598ef1fe49df6ab846176049df7605121cc5
dudwntjs <sts07190@naver.com>
2024-11-11 14:57:22 +0900

    Feat: 콘텐츠 생성 컴포넌트 #23

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,24 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React from 'react';
-// import MainPage from '../pages/MainPage';
+import MainPage from '../pages/MainPage';
 // import ContentAddPage from '../pages/ContentAddPage';
 // import SplashPage from '../pages/SplashPage';
 // import KakaoRedirect from '../components/api/KakaoRedirect';
 // import SignupPage from '../pages/SignupPage';
-// import ImageUploadComponent from '../components/ImageUpload';
-import PdfUploadComponent from '../components/PdfUpload';
-
+// import AddContent from '../components/AddContent';
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<SplashPage />} /> */}
-        {/* <Route path="/main" element={<MainPage />} /> */}
+        {/* {<Route path="/" element={<SplashPage />} />} */}
+        <Route path="/main" element={<MainPage />} />
         {/* <Route path="/signup" element={<SignupPage />} />
-        <Route path="/kakao/callback" element={<KakaoRedirect />} /> */}
-        {/* <Route path="/content-add" element={<ContentAddPage />} /> */}
-        {/* <Route path="/image-upload" element={<ImageUploadComponent />} /> */}
-        <Route path="/pdf-upload" element={<PdfUploadComponent />} />
+        <Route path="/kakao/callback" element={<KakaoRedirect />} />
+        <Route path="/content" element={<AddContent />} />
+        <Route path="/content-add" element={<ContentAddPage />} /> */}
       </Routes>
     </BrowserRouter>
   );

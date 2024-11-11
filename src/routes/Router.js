f083b2aa2d82bd9aab6ae41bf25eef0fb45b3e1f
dudwntjs <sts07190@naver.com>
2024-11-11 15:03:35 +0900

    Feat: 콘텐츠 생성 컴포넌트 #23

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,21 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import MainPage from '../pages/MainPage';
-// import ContentAddPage from '../pages/ContentAddPage';
-// import SplashPage from '../pages/SplashPage';
-// import KakaoRedirect from '../components/api/KakaoRedirect';
-// import SignupPage from '../pages/SignupPage';
-// import AddContent from '../components/AddContent';
+import SplashPage from '../pages/SplashPage';
+import KakaoRedirect from '../components/api/KakaoRedirect';
+import SignupPage from '../pages/SignupPage';
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* {<Route path="/" element={<SplashPage />} />} */}
+        {<Route path="/" element={<SplashPage />} />}
         <Route path="/main" element={<MainPage />} />
-        {/* <Route path="/signup" element={<SignupPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/kakao/callback" element={<KakaoRedirect />} />
-        <Route path="/content" element={<AddContent />} />
-        <Route path="/content-add" element={<ContentAddPage />} /> */}
       </Routes>
     </BrowserRouter>
   );

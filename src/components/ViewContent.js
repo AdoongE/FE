2fdf1338c31e554e05e708aeb300d1a78d858353
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2024-11-11 18:37:42 +0900

    Feat: 태그 박스 만들기 #30

--- src/components/ViewContent.js
+++ src/components/ViewContent.js
@@ -13,8 +13,8 @@ function ViewContent() {
           <TitleDiv>에어비엔비 광고 레퍼런스</TitleDiv>
           <ContentDiv>
             <Name>카테고리</Name>
-            <Category>카테고리1</Category>
-            <Category>카테고리2</Category>
+            <CategoryTag>카테고리1</CategoryTag>
+            <CategoryTag>카테고리2</CategoryTag>
           </ContentDiv>
           <ContentDiv>
             <Name>링크</Name>
@@ -37,43 +37,13 @@ function ViewContent() {
               https://www.youtube.com/watch?v=0cICRaWZFxk
             </Link>
           </ContentDiv>
-          <Tag>
-            <TagName>태그 (2개 이상)*</TagName>
-            {/* <TagInputs>
-              <TagContainer>
-                <Controller
-                  name="tags"
-                  control={control}
-                  defaultValue={[]}
-                  render={({ field, fieldState }) => (
-                    <>
-                      <TagInput
-                        label="tags"
-                        $error={fieldState.error ? true : undefined}
-                        $helperText={
-                          fieldState.error && fieldState.error.message
-                        }
-                        placeholder="태그 입력 후 엔터, 최대 8자, 띄어쓰기 불가"
-                        value={field.value || []}
-                        onChange={(newValue) => {
-                          field.onChange(newValue);
-                        }}
-                      />
-                      <InputButton onClick={() => showTagModal(field)}>
-                        + 태그 선택
-                      </InputButton>
-                      <AddTagModal
-                        ref={TagRef}
-                        onConfirm={(newCategory) => {
-                          field.onChange([...field.value, newCategory]);
-                        }}
-                      />
-                    </>
-                  )}
-                />
-              </TagContainer>
-            </TagInputs> */}
-          </Tag>
+          <ContentDiv>
+            <Name>태그 (2개 이상)*</Name>
+            <CategoryTag>광고</CategoryTag>
+            <CategoryTag>여행</CategoryTag>
+            <CategoryTag>에어비엔비</CategoryTag>
+            <CategoryTag>3D일러스트</CategoryTag>
+          </ContentDiv>
           <Dday>
             <Long>
               <Name className="dday">
@@ -95,57 +65,6 @@ function ViewContent() {
   );
 }
 
-// const InputButton = styled.button`
-//   width: fit-content;
-//   height: 44.19px;
-//   border-radius: 10px;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   border: 1px solid #9f9f9f;
-//   color: white;
-//   font-weight: 500;
-//   font-size: 20px;
-//   background-color: #9f9f9f;
-//   padding: 10px 16px;
-// `;
-
-// const TagContainer = styled.div`
-//   width: 794px;
-//   height: 60px;
-//   border: 1px solid #9f9f9f;
-//   border-radius: 10px;
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-//   z-index: 1;
-//   opacity: 1;
-//   padding-left: 12px;
-//   padding-right: 10px;
-// `;
-
-// const TagInputs = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   row-gap: 15px;
-// `;
-
-// const TagInput = styled.input`
-//   width: 625px;
-//   height: 50px;
-//   border: none;
-//   -webkit-appearance: none;
-//   appearance: none;
-//   overflow: auto;
-//   z-index: -1;
-
-//   &::placeholder {
-//     font-size: 20px;
-//     font-weight: 400;
-//     color: #4f4f4f;
-//     margin: 18px 18px;
-//   }
-// `;
 const ContentPage = styled.div`
   display: flex;
   flex-direction: column;
@@ -170,18 +89,19 @@ const ContentDiv = styled.div`
   align-items: center;
 `;
 
-const Category = styled.div`
+const CategoryTag = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   background-color: #41c3ab;
-  width: 111px;
+  width: auto;
   height: 44px;
   color: white;
   border: none;
   border-radius: 5px;
   font-size: 20px;
   margin-right: 20px;
+  padding: 0 16px;
 `;
 
 const Link = styled.div`
@@ -208,11 +128,6 @@ const Text = styled.textarea`
 `;
 
 const Dday = styled.div``;
-
-const Tag = styled.div`
-  display: flex;
-  column-gap: 41px;
-`;
 
 const Memo = styled.div`
   display: flex;
@@ -241,12 +156,6 @@ const Name = styled.div`
   &.dday {
     width: 550px;
   }
-`;
-
-const TagName = styled.div`
-  font-weight: 400;
-  font-size: 30px;
-  margin-top: 12px;
 `;
 
 const Button = styled.button`

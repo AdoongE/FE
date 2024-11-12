139bc3d7a09692fcd2ca3d0596eb3eed625b3b99
yelynnn <lynn1833@ewhain.net>
2024-11-12 13:07:53 +0900

    Feat: 콘텐츠 생성 파일 연결

--- src/pages/ContentAddPage.js
+++ src/pages/ContentAddPage.js
@@ -1,21 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Navbar from '../components/Navbar';
-import InitialContentAdd from '../components/InitialContentAdd';
 import AddContent from '../components/AddContent';
 
 function ContentAddPage() {
-  const [selectedContent, setSelectedContent] = useState(null);
-
-  const handleContentSelect = (type) => {
-    setSelectedContent(type);
-  };
-
   return (
     <div>
       <Navbar />
-      <InitialContentAdd onContentSelect={handleContentSelect} />
-      {/* 6번째 줄 에러 안 나려고 넣어놓음.. */}
-      {selectedContent === 'link'}
       <AddContent />
     </div>
   );

8a05736bce8f8dd1dbeed0534d8681f3ac559771
yelynnn <lynn1833@ewhain.net>
2024-11-30 20:34:27 +0900

    Fix: 머지 충돌 해결 #71

--- src/components/api/ContentAddApi.js
+++ src/components/api/ContentAddApi.js
@@ -1,39 +1,3 @@
-// import axios from 'axios';
-
-// const token = localStorage.getItem('jwtToken');
-
-// const formData = new FormData();
-// formData.append('request', JSON.stringify(request)); // JSON 데이터 추가
-
-// const api = axios.create({
-//   baseURL: 'http://52.78.221.255', // 백엔드 서버 주소로 설정
-//   headers: {
-//     Authorization: `${token}`, // 토큰을 템플릿 리터럴로 추가
-//   },
-// });
-
-// export const ContentAddHandler = async (data) => {
-//   // API 요청에 보낼 데이터 객체 생성
-//   const requestData = {
-//     ...data,
-//   };
-
-//   // API 요청에 보낼 데이터 전체 출력
-//   console.log('콘텐츠 생성 요청 데이터:', requestData);
-
-//   try {
-//     const response = await api.post('/api/v1/content/', requestData);
-//     if (response.data.status.code === 200) {
-//       console.log('콘텐츠 생성 성공: ', response.data.status.message);
-//       console.log(response.data.results.msg);
-//       return response;
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     throw error;
-//   }
-// };
-
 import axios from 'axios';
 
 const token = localStorage.getItem('jwtToken');

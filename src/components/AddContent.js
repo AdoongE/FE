e6f272945c508caa9a8dcb0ff26a5f1acbd74fa3
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2024-12-21 02:43:03 +0900

    Feat: 콘텐츠 생성 이미지, pdf api 연동 #76

--- src/components/AddContent.js
+++ src/components/AddContent.js
@@ -26,6 +26,7 @@ function AddContent({ onSetRepresentativeImage }) {
   const [tags, setTags] = useState([]);
   const [isComposing, setIsComposing] = useState(false); // 한국어 태그 이슈 해결을 위한
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [images, setImages] = useState([]);
   const navigate = useNavigate();
 
   const handleTagInput = (event) => {
@@ -47,10 +48,6 @@ function AddContent({ onSetRepresentativeImage }) {
       }
     }
   };
-
-  // const removeTag = (tagIdx) => {
-  //   setTags(tags.filter((tag, idx) => idx != tagIdx));
-  // };
 
   const ChangeRef = useRef(null);
 
@@ -195,11 +192,6 @@ function AddContent({ onSetRepresentativeImage }) {
     setPendingOption(null);
   };
 
-  // const handleDelete = (field, idx) => {
-  //   removeTag(idx);
-  //   onChange(tags.filter((item) => item !== field));
-  // };
-
   const handleCheckboxChange = (event) => {
     const option = event.target.name;
 
@@ -276,8 +268,7 @@ function AddContent({ onSetRepresentativeImage }) {
         updateData.thumbnailImage = representativeIndex; // 대표 이미지 포함
       }
 
-      console.log('콘텐츠 값', updateData);
-      ContentAddHandler(updateData);
+      ContentAddHandler(data.dataType, updateData, images);
       if (TagRef.current) {
         TagRef.current.resetTags();
       }
@@ -468,7 +459,11 @@ function AddContent({ onSetRepresentativeImage }) {
               />
             )}
             {dataType === 'IMAGE' && (
-              <ImageUploadComponent onSetRepresentative={handleImage} />
+              <ImageUploadComponent
+                onSetRepresentative={handleImage}
+                images={images}
+                setImages={setImages}
+              />
             )}
             {dataType === 'PDF' && (
               <PdfUploadComponent onSetRepresentative={handlePdf} />
--- src/components/api/ContentAddApi.js
+++ src/components/api/ContentAddApi.js
@@ -6,25 +6,47 @@ const api = axios.create({
   baseURL: 'http://52.78.221.255',
   headers: {
     Authorization: `${token}`,
+    'Content-Type': 'application/json',
+  },
+});
+const api_ = axios.create({
+  baseURL: 'http://52.78.221.255',
+  headers: {
+    Authorization: `${token}`,
+    'Content-Type': 'multipart/form-data',
   },
 });
 
-export const ContentAddHandler = async (data) => {
-  const formData = new FormData();
-  formData.append('request', JSON.stringify(data));
-
-  console.log('콘텐츠 생성 요청 데이터(FormData):', data);
-
+export const ContentAddHandler = async (dataType, data, images) => {
   try {
-    const response = await api.post('/api/v1/content/', formData);
+    const response = await api.post('/api/v1/content/', data);
 
     if (response.data.status.code === 200) {
-      console.log('콘텐츠 생성 성공:', response.data.status.message);
-      console.log('서버 응답 데이터:', response.data.results.msg);
+      console.log('콘텐츠 LINK 생성 성공:', response.data.status.message);
+
+      if (dataType !== 'LINK') {
+        try {
+          const contentsId = response.data.results[0].contentId;
+          const res = await api_.post(
+            `/api/v1/content/upload/${contentsId}`,
+            images,
+          );
+          if (res.data.status.code === 200) {
+            console.log('콘텐츠 file 생성 성공:', response.data.status.message);
+          }
+        } catch (error) {
+          console.error('콘텐츠 file 생성 중 오류 발생:', error);
+          throw error;
+        }
+      }
+
       return response;
+    } else {
+      console.log(response.data.status.code);
+      console.log('콘텐츠 LINK 생성 실패:', response.data.status.message);
     }
   } catch (error) {
-    console.error('콘텐츠 생성 중 오류 발생:', error);
+    console.error('콘텐츠 LINK 생성 중 오류 발생:', error);
     throw error;
   }
 };

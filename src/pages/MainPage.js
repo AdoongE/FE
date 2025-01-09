f757ba4aa26ff6147ad567096106ff83b86d0ff9
dudwntjs <sts07190@naver.com>
2025-01-10 02:44:47 +0900

    Design: 페이지네이션 위치 수정 #104

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -428,7 +428,7 @@ const Pagination = styled.div`
   gap: 8px;
   position: absolute;
   bottom: 30px; /* 화면 하단에서 20px 위로 띄움 */
-  left: 60%; /* 화면 가운데 정렬 */
+  left: 61%; /* 화면 가운데 정렬 */
   transform: translateX(-50%); /* 가운데 정렬 보정 */
   z-index: 10; /* 다른 요소보다 위로 표시되도록 */
 `;

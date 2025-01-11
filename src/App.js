d9db1cfa088361e5e1f01efac847c6266d76948c
dudwntjs <sts07190@naver.com>
2025-01-11 17:23:13 +0900

    Feat: px를 vw 변경 코드

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
+import React, { useEffect } from 'react';
 import { StyledEngineProvider } from '@mui/styled-engine';
 import Router from './routes/Router';
 import GlobalStyle from './styles/GlobalStyle';
 
+const convertPxToVw = () => {
+  const elements = document.querySelectorAll('*'); // 모든 요소 선택
+  elements.forEach((element) => {
+    const style = window.getComputedStyle(element);
+    const properties = [
+      'width',
+      'height',
+      'font-size',
+      'margin',
+      'padding',
+      'border-width',
+    ]; // px를 변환할 주요 속성들
+
+    properties.forEach((property) => {
+      const value = style[property];
+      if (value && value.includes('px')) {
+        const newValue = (parseInt(value) / window.innerWidth) * 100; // px를 vw로 변환
+        element.style[property] = `${newValue}vw`;
+
+        // 변환 확인을 위한 로그
+        console.log(`${property} - ${value} -> ${newValue}vw`);
+      }
+    });
+  });
+};
+
 function App() {
+  useEffect(() => {
+    // 페이지가 로드된 후 실행
+    convertPxToVw(); // 앱 로드 후 변환 함수 실행
+  }, []); // 컴포넌트가 처음 마운트될 때만 실행되도록 빈 배열을 사용
+
   return (
     <StyledEngineProvider injectFirst>
       <GlobalStyle />

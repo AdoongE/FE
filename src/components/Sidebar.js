c9e5b394ae9f08650c2cb19dbd3e9daf3d4836c3
dudwntjs <sts07190@naver.com>
2025-01-17 04:22:06 +0900

    사이드바 ‘나의 씨드’ 누를 때 씨드 전체조회

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -278,7 +278,14 @@ const Sidebar = ({
   };
 
   const handleTabClick = (tabName) => {
-    setActiveTab(tabName);
+    if (tabName === '리마인더') {
+      // "리마인더" 클릭 시 activeTab을 "리마인더"로 변경
+      setActiveTab('리마인더');
+    } else if (tabName === '나의 씨드') {
+      // "나의 씨드" 클릭 시 페이지 새로고침
+      setActiveTab('나의 씨드');
+      window.location.reload(); // 페이지 새로고침
+    }
   };
 
   const handleCategoryClick = (categoryName, listType) => {

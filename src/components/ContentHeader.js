5f8a2b04f89b528a3313874c47f31189064569d1
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2025-01-18 04:46:50 +0900

    Fix: contentheader 수정

--- src/components/ContentHeader.js
+++ src/components/ContentHeader.js
@@ -187,11 +187,11 @@ function ContentHeader({
 
   useEffect(() => console.log('선택한 태그: ', tags), [tags]);
 
-  useEffect(() => {
-    if (visibleTags.length === 0) {
-      setActiveTab('나의 씨드');
-    }
-  }, [visibleTags]);
+  // useEffect(() => {
+  //   if (visibleTags.length === 0) {
+  //     setActiveTab('나의 씨드');
+  //   }
+  // }, [visibleTags]);
 
   return (
     <Main>

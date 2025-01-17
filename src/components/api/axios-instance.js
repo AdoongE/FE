db6258e9d51373227b0e883e56d37dc717d144f3
김예린 <lynn1833@ewhain.net>
2025-01-18 02:23:31 +0900

    fix: api 원상복귀

--- src/components/api/axios-instance.js
+++ src/components/api/axios-instance.js
@@ -1,25 +1,11 @@
-// import axios from 'axios';
-
-// const token = localStorage.getItem('jwtToken');
-
-// const axiosInstance = axios.create({
-//   baseURL: `${process.env.REACT_APP_SERVER_URL}`,
-//   headers: {
-//     Authorization: `${token}`,
-//   },
-// });
-
-// export { axiosInstance };
-
 import axios from 'axios';
 
-const token =
-  'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzNzgzODAyMDg1IiwiYXV0aCI6IlVTRVIiLCJleHAiOjE3MzcxNDQxNjJ9.77u2w40RJQ66T-ZEa1e7TdkT-pGa7VfUbdE8CRneJMQ ';
+const token = localStorage.getItem('jwtToken');
 
 const axiosInstance = axios.create({
   baseURL: `${process.env.REACT_APP_SERVER_URL}`,
   headers: {
-    Authorization: `Bearer ${token}`,
+    Authorization: `${token}`,
   },
 });
 

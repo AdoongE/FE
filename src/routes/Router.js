b970da40e4a1f15ed29f0bc950494adc6ec39a50
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2025-03-09 01:32:39 +0900

    Init: 초기 환경 세팅

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,26 +1,25 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import MainPage from '../pages/MainPage';
-import ContentAddPage from '../pages/ContentAddPage';
-import SplashPage from '../pages/SplashPage';
-import KakaoRedirect from '../components/api/KakaoRedirect';
-import SignupPage from '../pages/SignupPage';
-import AddContent from '../components/AddContent';
-import ContentViewPage from '../pages/ContentViewPage';
-import ContentEditPage from '../pages/ContentEditPage';
-import MyPage from '../pages/MyPage';
-import ServiceConsent from '../pages/consents/ServiceConsent';
-import PersonalConsent from '../pages/consents/PersonalConsent';
-import MarketingConsent from '../pages/consents/MarketingConsent';
+// import ContentAddPage from '../pages/ContentAddPage';
+// import SplashPage from '../pages/SplashPage';
+// import KakaoRedirect from '../components/api/KakaoRedirect';
+// import SignupPage from '../pages/SignupPage';
+// import AddContent from '../components/AddContent';
+// import ContentViewPage from '../pages/ContentViewPage';
+// import ContentEditPage from '../pages/ContentEditPage';
+// import MyPage from '../pages/MyPage';
+// import ServiceConsent from '../pages/consents/ServiceConsent';
+// import PersonalConsent from '../pages/consents/PersonalConsent';
+// import MarketingConsent from '../pages/consents/MarketingConsent';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SplashPage />} />
+        {/* <Route path="/" element={<SplashPage />} /> */}
         <Route path="/main" element={<MainPage />} />
-        <Route path="/" element={<MainPage />} /> {/* "/" 도 MainPage로 연결 */}
-        <Route path="/signup" element={<SignupPage />} />
+        {/* <Route path="/signup" element={<SignupPage />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/kakao/callback" element={<KakaoRedirect />} />
         <Route path="/content" element={<AddContent />} />
@@ -29,7 +28,7 @@ const Router = () => {
         <Route path="/content-edit/:Id" element={<ContentEditPage />} />
         <Route path="/service-consent" element={<ServiceConsent />} />
         <Route path="/personal-consent" element={<PersonalConsent />} />
-        <Route path="/marketing-consent" element={<MarketingConsent />} />
+        <Route path="/marketing-consent" element={<MarketingConsent />} /> */}
       </Routes>
     </BrowserRouter>
   );

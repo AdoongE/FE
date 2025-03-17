0eebfaea5c0f28c3338e955d7e87ee84e7d6f38c
SunyoungJu <sts07190@naver.com>
2025-03-17 15:08:54 +0900

    feat: 드롭다운 세부 기능 수정한다.

--- src/components/Main/ContentHeader/Dropdowns/index.js
+++ src/components/Main/ContentHeader/Dropdowns/index.js
@@ -8,11 +8,10 @@ function Dropdowns() {
   const { resetDropdowns } = useDropdownStore();
   const dropdownRef = useRef(null);
 
-  // ✅ 컴포넌트 바깥 클릭 감지
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        resetDropdowns(); // ✅ 드롭다운 외부 클릭 시 기본값으로 변경
+        resetDropdowns();
       }
     };
 
--- src/components/Main/ContentHeader/Dropdowns/style.js
+++ src/components/Main/ContentHeader/Dropdowns/style.js
@@ -2,12 +2,17 @@ import styled from 'styled-components';
 
 export const DropdownsContainer = styled.div`
   display: flex;
-  gap: 8px; /* ✅ 드롭다운 간격 */
+  gap: 8px;
   align-items: center;
 `;
 
 export const DropdownContainer = styled.div`
   position: relative;
+  display: inline-block;
+
+  &:hover > div {
+    display: block;
+  }
 `;
 
 export const DropdownButton = styled.button`
@@ -18,15 +23,12 @@ export const DropdownButton = styled.button`
   border: 1px solid var(--gray3);
   border-radius: 8px;
   font-size: 14px;
-  color: ${(props) => (props.isDefault ? 'var(--gray2)' : '#333')};
+  color: var(--gray2);
   cursor: pointer;
-  padding: 12px 16px; /* 🔥 버튼 내부 패딩 */
+  padding: 12px 16px;
 
   width: ${(props) => (props.$isSort ? '106px' : '137px')}
   box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
-
-  &:hover {
-    background-color: var(--gray4);
   }
 
   .dropdown-icon {
@@ -46,12 +48,18 @@ export const DropdownMenu = styled.div`
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   z-index: 10;
   width: ${(props) => (props.$isSort ? '106px' : '137px')};
+  display: none; /* ✅ 기본적으로 숨김 */
+
+  /* ✅ 호버 시 드롭다운 표시 */
+  ${DropdownContainer}:hover & {
+    display: block;
+  }
 `;
 
 export const DropdownItem = styled.div`
   padding: 12px 16px;
   font-size: 14px;
-  color: var(--gray1);
+  color: var(--gray2);
   cursor: pointer;
   text-align: left;
   display: flex;

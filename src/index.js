23f1ff37060c3f472d9fcacfe360c61b5497cd72
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2025-03-20 16:00:58 +0900

    init: font, globalstyle 초기 설정

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import GlobalStyle from './styles/GlobalStyle';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
 );
---
+++ src/styles/font.js
@@ -0,0 +1,36 @@
+import { css } from 'styled-components';
+
+export const font = {
+  title0: css`
+    font-size: 28px;
+    font-weight: 600;
+  `,
+  title1: css`
+    font-size: 20px;
+    font-weight: 600;
+  `,
+  title2: css`
+    font-size: 16px;
+    font-weight: 600;
+  `,
+  title3: css`
+    font-size: 16px;
+    font-weight: 500;
+  `,
+  title4: css`
+    font-size: 16px;
+    font-weight: 400;
+  `,
+  body1: css`
+    font-size: 12px;
+    font-weight: 500;
+  `,
+  body2: css`
+    font-size: 12px;
+    font-weight: 400;
+  `,
+  body3: css`
+    font-size: 8px;
+    font-weight: 400;
+  `,
+};
